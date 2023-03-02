32c1bde77022265f48a46039d11bb5621e512b14
Vockenroth <judith.vockenroth@gmail.com>
2023-03-02 09:44:47 +0100

    Some test refractoring

--- test/graph_parser_test.py
+++ test/graph_parser_test.py
@@ -7,19 +7,29 @@ from src.graph_parser import GraphParser
 resources_dir = getcwd() + '/../resources'
 graph_file_path = resources_dir + '/ophois-graph.txt'
 
-MOCK_INIT_POINT = '6845757797'
-MOCK_END_POINT = '1238436031'
+TEST_INIT_POINT = '6845757797'
+TEST_END_POINT = '1238436031'
+TEST_PATH_POSSIBLE_RESULT_1 = ['6845757797', '6845757796', '10275176304', '10275291415', '10275290094', '1238436031']
+TEST_PATH_POSSIBLE_RESULT_2 = ['6845757797', '6845757796', '10275176304', '10275295121', '10275290094', '1238436031']
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_shortest_distance_is_correct(self):
         parser = GraphParser(graph_file_path)
         graph = parser.parse_simplified_map_to_graph()
-        dijkstra = DijkstraSPF(graph, MOCK_INIT_POINT)
+        dijkstra = DijkstraSPF(graph, TEST_INIT_POINT)
 
-        print(dijkstra.get_distance(MOCK_END_POINT))
+        self.assertEqual(dijkstra.get_distance(TEST_END_POINT), 5)
 
-        self.assertEqual(dijkstra.get_distance(MOCK_END_POINT), 5)
+    # TODO: find an example where the result is always the same
+    # def test_get_right_path(self):
+    #     parser = GraphParser(graph_file_path)
+    #     graph = parser.parse_simplified_map_to_graph()
+    #     dijkstra = DijkstraSPF(graph, TEST_INIT_POINT)
+    #
+    #     print(dijkstra.get_path(TEST_END_POINT))
+    #
+    #     self.assertListEqual(dijkstra.get_path(TEST_END_POINT), TEST_PATH_POSSIBLE_RESULT_1)
 
 
 if __name__ == '__main__':
