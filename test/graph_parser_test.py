4a261e011a849b2419b440ee7304982c86d97a80
Vockenroth <judith.vockenroth@gmail.com>
2023-03-03 16:37:47 +0100

    Make graph parser tests pytests

--- test/graph_parser_test.py
+++ test/graph_parser_test.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 from os import getcwd
 from dijkstra import DijkstraSPF
 
@@ -14,24 +14,19 @@ TEST_PATH_POSSIBLE_RESULT_1 = ['6845757797', '6845757796', '10275176304', '10275
 TEST_PATH_POSSIBLE_RESULT_2 = ['6845757797', '6845757796', '10275176304', '10275295121', '10275290094', '1238436031']
 
 
-class MyTestCase(unittest.TestCase):
-    def test_shortest_distance_is_correct(self):
-        parser = GraphParser(graph_file_path, map_file_path)
-        graph = parser.parse_simplified_map_to_graph()
-        dijkstra = DijkstraSPF(graph, TEST_INIT_POINT)
-
-        self.assertEqual(5, dijkstra.get_distance(TEST_END_POINT))
-
-    # TODO: find an example where the result is always the same
-    # def test_get_right_path(self):
-    #     parser = GraphParser(graph_file_path)
-    #     graph = parser.parse_simplified_map_to_graph()
-    #     dijkstra = DijkstraSPF(graph, TEST_INIT_POINT)
-    #
-    #     print(dijkstra.get_path(TEST_END_POINT))
-    #
-    #     self.assertListEqual(dijkstra.get_path(TEST_END_POINT), TEST_PATH_POSSIBLE_RESULT_1)
+@pytest.fixture()
+def initialise_graph():
+    parser = GraphParser(graph_file_path, map_file_path)
+    graph = parser.parse_simplified_map_to_graph()
+    dijkstra = DijkstraSPF(graph, TEST_INIT_POINT)
+    return dijkstra
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_shortest_distance_is_correct(initialise_graph):
+    assert 5 == initialise_graph.get_distance(TEST_END_POINT)
+
+
+# TODO: find an example where the result is always the same
+def test_get_right_path(initialise_graph):
+    assert initialise_graph.get_path(TEST_END_POINT) == TEST_PATH_POSSIBLE_RESULT_1 \
+           or initialise_graph.get_path(TEST_END_POINT) == TEST_PATH_POSSIBLE_RESULT_2
