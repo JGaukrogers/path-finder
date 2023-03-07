2d7ae8e17ae72e7ca680dcc131349157192ccd0c
Vockenroth <judith.vockenroth@gmail.com>
2023-03-07 22:28:24 +0100

    Start building skeleton for displaying map

--- src/display_map.py
+++ src/display_map.py
@@ -1,8 +1,26 @@
+from os import getcwd
+from dijkstra import DijkstraSPF
+
 import folium
+
 # from folium.plugins import MarkerCluster
 # import pandas as pd
+from src.graph_parser import GraphParser
+
+resources_dir = getcwd() + '/../resources'
+MOCK_GRAPH_FILE = resources_dir + '/ophois-graph.txt'
+MOCK_MAP_FILE = resources_dir + '/my_town.osm'
 
 
+def get_quietest_way(start_node_id: str, end_node_id: str):
+    graph_parser = GraphParser(MOCK_GRAPH_FILE, MOCK_MAP_FILE)
+    weighed_graph = graph_parser.parse_simplified_map_to_graph()
+    dijkstra = DijkstraSPF(weighed_graph, start_node_id)
+    # Print list of nodes in "lightest" way
+    print(dijkstra.get_path(end_node_id))
+
+
+'''
 lat="41.7889938"
 lon="0.8188493"
 
@@ -35,3 +53,8 @@ folium.PolyLine(trail_coordinates, tooltip="Coast").add_to(my_map)
 
 # Save the map
 my_map.save("index.html")
+'''
+
+TEST_INIT_POINT = '6845757797'
+TEST_END_POINT_SHORT = '6845757796'
+get_quietest_way(TEST_INIT_POINT, TEST_END_POINT_SHORT)
