254b60075eaef785e4df8b7cefc127f3fb6456ae
Vockenroth <judith.vockenroth@gmail.com>
2023-03-08 22:22:54 +0100

    Review

--- src/node_info.py
+++ src/node_info.py
@@ -1,17 +1,9 @@
 from src.way import Way
+from dataclasses import dataclass
 
 
+@dataclass
 class NodeInfo:
-    def __init__(self):
-        self.ways = set()
-        self.lat = None
-        self.lon = None
-
-    def get_connected_ways(self):
-        return self.ways
-
-    def add_way(self, way: Way):
-        self.ways.add(way)
-
-    def __repr__(self):
-        return f'lat: {self.lat} lon: {self.lon} ways: {self.ways}'
+    ways: set[Way]
+    lat: float
+    lon: float
