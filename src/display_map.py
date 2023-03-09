41a7eb646f864e281b89483db1293640d8d8c1bb
Vockenroth <judith.vockenroth@gmail.com>
2023-03-09 19:22:01 +0100

    Put functions of display_map into a class and create test file

--- src/display_map.py
+++ src/display_map.py
@@ -1,55 +1,45 @@
-from os import getcwd
+import folium
 from dijkstra import DijkstraSPF
 
-import folium
-
-# from folium.plugins import MarkerCluster
-# import pandas as pd
 from src.graph_parser import GraphParser
 
-resources_dir = getcwd() + '/../resources'
-MOCK_GRAPH_FILE = resources_dir + '/ophois-graph.txt'
-MOCK_MAP_FILE = resources_dir + '/my_town.osm'
 
+class MapDisplayer:
 
-def get_quietest_way(start_node_id: str, end_node_id: str):
-    graph_parser = GraphParser(MOCK_GRAPH_FILE, MOCK_MAP_FILE)
-    weighed_graph = graph_parser.parse_simplified_map_to_graph()
-    dijkstra = DijkstraSPF(weighed_graph, start_node_id)
+    def __init__(self, graph_parser: GraphParser, dijkstra: DijkstraSPF):
+        self.graph_parser = graph_parser
+        self.dijkstra = dijkstra
 
-    start_node_coords = get_node_coordinates(graph_parser, start_node_id)
-    end_node_coords = get_node_coordinates(graph_parser, end_node_id)
+    def get_quietest_way(self, start_node_id: str, end_node_id: str):
+        start_node_coords = self.get_node_coordinates(start_node_id)
+        end_node_coords = self.get_node_coordinates(end_node_id)
 
-    map = folium.Map(location=start_node_coords, zoom_start=17)
+        map = folium.Map(location=start_node_coords, zoom_start=17)
 
-    folium.Marker(start_node_coords, popup='Start').add_to(map)
-    folium.Marker(end_node_coords, popup='End').add_to(map)
+        folium.Marker(start_node_coords, popup='Start').add_to(map)
+        folium.Marker(end_node_coords, popup='End').add_to(map)
 
-    trail_coordinates = get_trail_coordinates(dijkstra.get_path(end_node_id), graph_parser)
+        trail_coordinates = self.get_trail_coordinates(self.dijkstra.get_path(end_node_id))
 
-    folium.PolyLine(trail_coordinates, tooltip="Coast").add_to(map)
+        folium.PolyLine(trail_coordinates, tooltip="Coast").add_to(map)
 
-    map.save("index.html")
+        map.save("index.html")
 
+        print(map.to_dict())
+        for elem in map.to_dict():
+            print(elem)
 
-def get_node_coordinates(graph_parser, start_node_id):
-    start_node_info = graph_parser.nodeId_to_nodeInfo_dict[start_node_id]
-    lat = start_node_info.lat
-    lon = start_node_info.lon
-    start_node_coords = [lat, lon]
-    return start_node_coords
+    def get_node_coordinates(self, start_node_id):
+        start_node_info = self.graph_parser.nodeId_to_nodeInfo_dict[start_node_id]
+        lat = start_node_info.lat
+        lon = start_node_info.lon
+        start_node_coords = [lat, lon]
+        return start_node_coords
 
-
-def get_trail_coordinates(node_list, graph_parser):
-    trail_coordinates = []
-    for path_points in node_list:
-        lat = graph_parser.nodeId_to_nodeInfo_dict[path_points].lat
-        lon = graph_parser.nodeId_to_nodeInfo_dict[path_points].lon
-        trail_coordinates.append((lat, lon))
-    return trail_coordinates
-
-
-TEST_INIT_POINT = '6845757797'
-TEST_END_POINT_SHORT = '6845757796'
-TEST_END_POINT_LONG = '1238435933'
-get_quietest_way(TEST_INIT_POINT, TEST_END_POINT_LONG)
+    def get_trail_coordinates(self, node_list):
+        trail_coordinates = []
+        for path_points in node_list:
+            lat = self.graph_parser.nodeId_to_nodeInfo_dict[path_points].lat
+            lon = self.graph_parser.nodeId_to_nodeInfo_dict[path_points].lon
+            trail_coordinates.append((lat, lon))
+        return trail_coordinates
---
+++ test/test_display_map.py
@@ -0,0 +1,33 @@
+from os import getcwd
+
+from dijkstra import DijkstraSPF
+
+from src.display_map import MapDisplayer
+from src.graph_parser import GraphParser
+
+resources_dir = getcwd() + '/../resources'
+MOCK_GRAPH_FILE = resources_dir + '/ophois-graph.txt'
+MOCK_MAP_FILE = resources_dir + '/my_town.osm'
+
+
+TEST_INIT_POINT = '6845757797'
+TEST_END_POINT_SHORT = '6845757796'
+TEST_END_POINT_LONG = '1238435933'
+
+
+def test_create_graph_one_edge():
+    test_graph_parser = GraphParser(MOCK_GRAPH_FILE, MOCK_MAP_FILE)
+    test_weighed_graph = test_graph_parser.parse_simplified_map_to_graph()
+    test_dijkstra = DijkstraSPF(test_weighed_graph, TEST_INIT_POINT)
+
+    map_displayer = MapDisplayer(test_graph_parser, test_dijkstra)
+    map_displayer.get_quietest_way(TEST_INIT_POINT, TEST_END_POINT_SHORT)
+
+
+def test_create_graph_four_edges():
+    test_graph_parser = GraphParser(MOCK_GRAPH_FILE, MOCK_MAP_FILE)
+    test_weighed_graph = test_graph_parser.parse_simplified_map_to_graph()
+    test_dijkstra = DijkstraSPF(test_weighed_graph, TEST_INIT_POINT)
+
+    map_displayer = MapDisplayer(test_graph_parser, test_dijkstra)
+    map_displayer.get_quietest_way(TEST_INIT_POINT, TEST_END_POINT_LONG)
