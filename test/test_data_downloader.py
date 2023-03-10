2945fba1011d777d425afc04951d255621e9bebf
Bob Belderbos <bobbelderbos@gmail.com>
2023-03-10 17:11:12 +0100

    coding session

--- test/test_data_downloader.py
+++ test/test_data_downloader.py
@@ -1,37 +1,48 @@
 import subprocess
+from pathlib import Path
+from sys import platform
 
 import pytest
 import os
 
 from src.map_downloader import DataDownloader
 
-TEST_VILLAGE = 'Taurinya'
-TEST_SIMPLIFIED_GRAPH = f'{TEST_VILLAGE}-simplified.graph'
-run_ophois_cmd_good = os.getcwd() + './../bin/ophois'
-run_ophois_cmd_bad = os.getcwd() + './ophoisAAAA'
+TEST_VILLAGE = "Taurinya"
+TEST_SIMPLIFIED_GRAPH = f"{TEST_VILLAGE}-simplified.graph"
+
+run_ophois_cmd_good = "./bin/ophois"
+run_ophois_cmd_bad = "./ophoisAAAA"
 
 
 @pytest.fixture()
-def data_downloader():
-    data_downloader = DataDownloader(TEST_VILLAGE, run_ophois_cmd_good)
-    return data_downloader
+def rm_graph_file():
+    try:
+        Path(TEST_SIMPLIFIED_GRAPH).unlink()
+    except FileNotFoundError:
+        pass
 
 
-def remove_test_graph_file():
-    if os.path.exists(TEST_SIMPLIFIED_GRAPH):
-        subprocess.check_output(f'rm {TEST_SIMPLIFIED_GRAPH}', shell=True)
+@pytest.fixture()
+def data_downloader(rm_graph_file):
+    return DataDownloader(TEST_VILLAGE, ophois=run_ophois_cmd_good)
 
 
+@pytest.fixture()
+def data_downloader_bad(rm_graph_file):
+    return DataDownloader(TEST_VILLAGE, ophois=run_ophois_cmd_bad)
+
+
+def _is_linux():
+    return platform.startswith("linux")
+
+
+@pytest.mark.skipif(not _is_linux(), reason="Only run on Linux")
 def test_data_download_good(data_downloader):
-    data_downloader = DataDownloader(TEST_VILLAGE)
-    remove_test_graph_file()
-    data_downloader.get_simplified_graph()
+    assert data_downloader.get_simplified_graph()
     assert os.path.isfile(TEST_SIMPLIFIED_GRAPH)
     assert os.path.getsize(TEST_SIMPLIFIED_GRAPH) > 0
 
 
-def test_ophois_not_available():
-    data_downloader = DataDownloader(TEST_VILLAGE, run_ophois_cmd_bad)
-    remove_test_graph_file()
-    assert not data_downloader.get_simplified_graph()
+def test_ophois_not_available(data_downloader_bad):
+    assert not data_downloader_bad.get_simplified_graph()
     assert not os.path.exists(TEST_SIMPLIFIED_GRAPH)
