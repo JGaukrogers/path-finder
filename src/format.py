966aa57d1c2f139429199873d21f49ddbae08f8b
Vockenroth <judith.vockenroth@gmail.com>
2023-08-17 20:42:01 +0200

    Separate read nodes and ways in separate lines

--- src/format.py
+++ src/format.py
@@ -8,8 +8,7 @@ from src.graph_parser import SEPARATOR
 def extract_and_write_to_file(in_file, out_file):
     extracted_data = format_json_to_graph(in_file)
     with open(out_file, 'w') as f:
-        for line in extracted_data:
-            f.write(line)
+        f.writelines(extracted_data)
 
 
 def format_json_to_graph(file_name: str) -> list[str]:
@@ -29,13 +28,13 @@ def extract_way(element):
     node_list = element['nodes']
     pairs = pairwise(node_list)
     for p in pairs:
-        line = f'{p[0]}{SEPARATOR}{p[1]}'
+        line = f'{p[0]}{SEPARATOR}{p[1]}\n'
         pair_list.append(line)
     return pair_list
 
 
 def extract_node(element):
-    return f'{element["id"]}{SEPARATOR}{element["lat"]}{SEPARATOR}{element["lon"]}'
+    return f'{element["id"]}{SEPARATOR}{element["lat"]}{SEPARATOR}{element["lon"]}\n'
 
 
 # parsed_graph = format_json_to_graph('test_json.osm')
