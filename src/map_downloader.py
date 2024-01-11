bb0cedba4c9b918367cd39993b6182946e9c9474
Vockenroth <judith.vockenroth@gmail.com>
2024-01-11 12:35:36 +0100

    23 - Add check for failed connection

--- src/map_downloader.py
+++ src/map_downloader.py
@@ -1,6 +1,6 @@
 import requests
 
-import src.constants as constants
+from src.constants import OSM_FILENAME_TEMPLATE, EXTRACTED_GRAPH_FILENAME_TEMPLATE, OVERPASS_QUERY, OVERPASS_URL
 from src.format import extract_and_write_to_file
 
 
@@ -8,20 +8,24 @@ class DataDownloader:
 
     def __init__(self, file_name: str, area_boundaries: dict):
         self.area_boundaries = area_boundaries
-        self.osm_file = constants.OSM_FILENAME_TEMPLATE.format(file_name=file_name)
-        self.extracted_graph = constants.EXTRACTED_GRAPH_FILENAME_TEMPLATE.format(file_name=file_name)
+        self.osm_file = OSM_FILENAME_TEMPLATE.format(file_name=file_name)
+        self.extracted_graph = EXTRACTED_GRAPH_FILENAME_TEMPLATE.format(file_name=file_name)
 
-    def get_simplified_graph(self):
-        self.get_map_data()
-        extract_and_write_to_file(self.osm_file, self.extracted_graph)
-        return True
+    def get_simplified_graph(self) -> bool:
+        if self.get_map_data():
+            extract_and_write_to_file(self.osm_file, self.extracted_graph)
+            return True
+        return False
 
-    def get_map_data(self):
-        overpass_query = constants.OVERPASS_QUERY.format(n=self.area_boundaries['north'],
-                                                         s=self.area_boundaries['south'],
-                                                         e=self.area_boundaries['east'],
-                                                         w=self.area_boundaries['west'])
-        response = requests.get(constants.OVERPASS_URL,
-                                params={'data': overpass_query})
+    def get_map_data(self) -> bool:
+        overpass_query = OVERPASS_QUERY.format(n=self.area_boundaries['north'],
+                                               s=self.area_boundaries['south'],
+                                               e=self.area_boundaries['east'],
+                                               w=self.area_boundaries['west'])
+        try:
+            response = requests.get(OVERPASS_URL, params={'data': overpass_query})
+        except requests.RequestException | requests.ConnectionError | requests.HTTPError | requests.Timeout:
+            return False
         with open(self.osm_file, 'w') as fd:
             fd.write(response.text)
+        return True
