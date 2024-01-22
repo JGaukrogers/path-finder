9a7de6e13aa950674c132429ba7f66dbbfd71deb
Vockenroth <judith.vockenroth@gmail.com>
2024-01-22 18:49:04 +0100

    29 - add home path as constant and update requirements

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,7 @@
+import os
 from collections import namedtuple
+from dotenv import load_dotenv
+from pathlib import Path
 
 MapPoint = namedtuple('MapPoint', 'lat lon')
 AreaBoundaries = namedtuple('AreaBoundaries', 'north south east west')
@@ -13,10 +16,16 @@ way({s},{w},{n},{e})[highway];
 out body;
 '''
 
+dotenv_path = Path('.env')
+load_dotenv(dotenv_path=dotenv_path)
+PF_HOME = os.environ['PF_HOME']
+
 EXTRACTED_GRAPH_FILENAME_TEMPLATE = '{file_name}-extracted.graph'
 OSM_FILENAME_TEMPLATE = '{file_name}.osm'
 HTML_OUTFILE = '{file_name}.html'
-HTML_OUTPATH = 'src/templates/{file_name}.html'
+
+HOME = os.environ['PF_HOME']
+HTML_OUTPATH = HOME + 'src/templates/{file_name}.html'
 
 PRIORITY_QUIETNESS = 'quietness'
 PRIORITY_SHORT_DISTANCE = 'distance'
