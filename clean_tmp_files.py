c788443b46263a8e39ef2d899ad01fc97c70f82c
Vockenroth <judith.vockenroth@gmail.com>
2024-01-23 18:13:25 +0100

    29 - adapt clean files script

--- clean_tmp_files.py
+++ clean_tmp_files.py
@@ -1,19 +1,19 @@
 import glob
 import os
 
-from src.constants import PF_HOME
+from src.constants import DOWNLOADS_HOME, TEMPLATES_HOME
 
-extentions_to_remove = ['*.osm', '*.graph', '*.html']
+extentions_to_remove = ['*.osm', '*.graph']
 
 # Downloaded and generated files in /
 for extension in extentions_to_remove:
-    files_to_remove = glob.iglob(PF_HOME + extension)
+    files_to_remove = glob.iglob(str(DOWNLOADS_HOME) + '/' + extension)
     for file in files_to_remove:
         os.remove(file)
 
 # Generated HTML files in templates
 html_files_whitelist = ['base.html', 'index.html', 'content.html']
-files_to_remove = glob.iglob(PF_HOME + 'src/templates/' + '*.html')
+files_to_remove = glob.iglob(str(TEMPLATES_HOME) + '/' + '*.html')
 for file in files_to_remove:
     if file.split('/')[-1] not in html_files_whitelist:
         os.remove(file)
