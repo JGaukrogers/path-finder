25c6bdb96485032be5e69b7f232be0b4e885953f
Vockenroth <judith.vockenroth@gmail.com>
2024-02-06 12:59:28 +0100

    40 - Fix tests in test_display_map.py and parametrize

--- test/test_display_map.py
+++ test/test_display_map.py
@@ -1,11 +1,12 @@
+import os
+
 import pytest
 from dijkstra import DijkstraSPF
 
 from src.constants import PRIORITY_QUIETNESS
 from src.display_map import MapDisplayer
 from src.graph_parser import GraphParser
-from test.test_constants import TEST_INIT_POINT_ID, TEST_END_POINT_SHORT_ID, TEST_END_POINT_LONG_ID, \
-    MOCK_MAP_CONTENT, MOCK_GRAPH_CONTENT, \
+from test.test_constants import TEST_INIT_POINT_ID, TEST_END_POINT_SHORT_ID, MOCK_MAP_CONTENT, MOCK_GRAPH_CONTENT, \
     INDEX_SHORT_HTML, INDEX_LONG_HTML
 
 test_graph_parser = GraphParser(MOCK_GRAPH_CONTENT, MOCK_MAP_CONTENT, PRIORITY_QUIETNESS)
@@ -20,9 +21,14 @@ def map_displayer():
     return MapDisplayer(test_graph_parser, test_dijkstra)
 
 
-def test_create_graph_one_edge(map_displayer):
-    map_displayer.generate_map(TEST_INIT_POINT_ID, TEST_END_POINT_SHORT_ID, INDEX_SHORT_HTML)
+testdata = [
+    INDEX_SHORT_HTML,
+    INDEX_LONG_HTML,
+]
 
 
-def test_create_graph_four_edges(map_displayer):
-    map_displayer.generate_map(TEST_INIT_POINT_ID, TEST_END_POINT_LONG_ID, INDEX_LONG_HTML)
+@pytest.mark.parametrize('html_filename', testdata)
+def test_create_graph_one_edge(map_displayer, html_filename):
+    map_displayer.generate_map(TEST_INIT_POINT_ID, TEST_END_POINT_SHORT_ID, html_filename)
+    assert os.path.isfile(INDEX_SHORT_HTML)
+    assert os.path.getsize(INDEX_SHORT_HTML) > 0
